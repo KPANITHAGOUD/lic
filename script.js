0576be486886de56b5886b0f361c79325919ca53
KPANITHAGOUD <kp.anitha.goud@gmail.com>
2025-03-06 22:00:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".cta-button").addEventListener("click", function () {
+        alert("Contact us for more details!");
+    });
+});
